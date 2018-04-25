e46e15caf7df457ce730634585f03e21fdcd6823
yixiong.luo <yixiong.luo@brightcells.com>
2018-04-25 18:49:34 +0800

    ...

--- app/src/main/java/yincheng/sourcecodeinvestigate/ui/activity/LruCacheActivity.java
+++ app/src/main/java/yincheng/sourcecodeinvestigate/ui/activity/LruCacheActivity.java
@@ -1,9 +1,9 @@
 package yincheng.sourcecodeinvestigate.ui.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 import yincheng.sourcecodeinvestigate.R;
 
@@ -12,9 +12,12 @@ import yincheng.sourcecodeinvestigate.R;
  * github:luoyincheng
  */
 public class LruCacheActivity extends AppCompatActivity {
+    private ImageView imageView;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_regex);
+        imageView = findViewById(R.id.iv_cache);
     }
 }
