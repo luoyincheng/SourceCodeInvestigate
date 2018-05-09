bcfabc0f253006d110eb0e0f54227b767608f9a2
yixiong.luo <yixiong.luo@brightcells.com>
2018-05-09 18:18:39 +0800

    handler

---
+++ app/src/main/java/yincheng/sourcecodeinvestigate/localbroadcastmanager/LocalBroadcastManager.java
@@ -0,0 +1,9 @@
+package yincheng.sourcecodeinvestigate.localbroadcastmanager;
+
+/**
+ * Created by yincheng on 2018/5/9/16:26.
+ * github:luoyincheng
+ */
+public class LocalBroadcastManager {
+    android.support.v4.content.LocalBroadcastManager
+}
