acf5350bd52d49501282cd3e7b7809f5a2e42f3b
luoyincheng <yincheng.luo@gmail.com>
2018-05-16 08:41:07 +0800

    del

--- app/src/main/java/yincheng/sourcecodeinvestigate/androidinterviewpoint/absctractfactory/King.java
+++
@@ -1,9 +0,0 @@
-package yincheng.sourcecodeinvestigate.androidinterviewpoint.absctractfactory;
-
-/**
- * Created by yincheng on 2018/5/15/18:42.
- * github:luoyincheng
- */
-public interface King {
-    String getDescription();
-}
